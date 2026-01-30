9a7782abaa78e42e4e341a8a0eb1d6956dc12d5d
Jorge Ortiz <jortizb96@gmail.com>
2026-01-30 16:13:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,31 +5,31 @@ function home() {
 // Datos del menú
 const menuData = {
     refrescos: [
-        { name: 'Agua', description: '330ml', price: '1.50€', image: 'images/agua.jpg' },
-        { name: 'Coca-Cola', description: '330ml', price: '2.40€', image: 'images/coca.jpg' },
-        { name: 'Coca-Cola Zero', description: '330ml', price: '2.40€', image: 'images/cocazero.jpg' },
-        { name: 'Fanta Limón', description: '330ml', price: '2.40€', image: 'images/fantalimon.jpg' },
-        { name: 'Fanta Naranja', description: '330ml', price: '2.40€', image: 'images/fantanaranja.jpg' },
-        { name: 'Sprite', description: '330ml', price: '2.40€', image: 'images/sprite.jpg' },
-        { name: 'Nestea Maracuyá', description: '330ml', price: '2.40€', image: 'images/nestea.jpg' },
-        { name: 'Aquarius', description: '330ml', price: '2.40€', image: 'images/aquarius.jpg' },
-        { name: 'Schweppes Tónica', description: '330ml', price: '2.40€', image: 'images/tonica.jpg' },
-        { name: 'Monster Energy', description: '500ml', price: '3.00€', image: 'images/monster.jpg' },
-        { name: 'Monster Energy Ultra', description: '500ml', price: '3.00€', image: 'images/monsterblanco.jpg' },
-        { name: 'Monster Energy Mango Loco', description: '500ml', price: '3.00€', image: 'images/monstermango.jpg' },
-        { name: 'Monster Energy Manzana Ultra Paradise', description: '500ml', price: '3.00€', image: 'images/monstermanzana.jpg' },
-        { name: 'Monster Energy Ultra Peachy Keen', description: '500ml', price: '3.00€', image: 'images/monstermelocoton.jpg' },
-        { name: 'Monster Energy Rehub Recover', description: '500ml', price: '3.00€', image: 'images/rehab.jpg' },
+        { name: 'Agua', description: '330 ml', price: '1.50€', image: 'images/agua.jpg' },
+        { name: 'Coca-Cola', description: '330 ml', price: '2.40€', image: 'images/coca.jpg' },
+        { name: 'Coca-Cola Zero', description: '330 ml', price: '2.40€', image: 'images/cocazero.jpg' },
+        { name: 'Fanta Limón', description: '330 ml', price: '2.40€', image: 'images/fantalimon.jpg' },
+        { name: 'Fanta Naranja', description: '330 ml', price: '2.40€', image: 'images/fantanaranja.jpg' },
+        { name: 'Sprite', description: '330 ml', price: '2.40€', image: 'images/sprite.jpg' },
+        { name: 'Nestea Maracuyá', description: '330 ml', price: '2.40€', image: 'images/nestea.jpg' },
+        { name: 'Aquarius', description: '330 ml', price: '2.40€', image: 'images/aquarius.jpg' },
+        { name: 'Schweppes Tónica', description: '330 ml', price: '2.40€', image: 'images/tonica.jpg' },
+        { name: 'Monster Energy', description: '500 ml', price: '3.00€', image: 'images/monster.jpg' },
+        { name: 'Monster Energy Ultra', description: '500 ml', price: '3.00€', image: 'images/monsterblanco.jpg' },
+        { name: 'Monster Energy Mango Loco', description: '500 ml', price: '3.00€', image: 'images/monstermango.jpg' },
+        { name: 'Monster Energy Manzana Ultra Paradise', description: '500 ml', price: '3.00€', image: 'images/monstermanzana.jpg' },
+        { name: 'Monster Energy Ultra Peachy Keen', description: '500 ml', price: '3.00€', image: 'images/monstermelocoton.jpg' },
+        { name: 'Monster Energy Rehub Recover', description: '500 ml', price: '3.00€', image: 'images/rehab.jpg' },
     ],
     cervezas: {
         'De Barril': [
-            { name: 'Kalimotxo', description: '500 ml', price: '3.50€', image: 'images/barrilmahou.jpg' },
+            { name: 'Kalimotxo', description: '500 ml', price: '3.50€', image: 'images/kalimotxo.jpg' },
             { name: 'Mahou Clásica', description: 'Caña/Pinta', price: '1.80€/3.50€', image: 'images/barrilmahou.jpg' },
             { name: 'Clara Mahou Clásica', description: 'Caña/Pinta', price: '1.80€/3.50€', image: 'images/barrilmahou.jpg' },
-            { name: 'Mahou Maestra', description: 'Caña/Pinta', price: '2.20€/4.00€', image: 'images/barrilmaestra.jpg' },
-            { name: 'Alhambra Roja', description: 'Caña/Pinta', price: '2.50€/4.50€', image: 'images/barrilroja.jpg' },
-            { name: 'Founders Porter', description: 'Caña/Pinta', price: '3.00€/5.50€', image: 'images/barrilporter.jpg' },
-            { name: 'Franziskaner', description: 'Caña/Pinta', price: '2.20€/4.00€', image: 'images/barrilfranziskaner.jpg' }
+            { name: 'Mahou Maestra Doble Lúpulo', description: 'Caña/Pinta', price: '2.20€/4.00€', image: 'images/maestra.jpg' },
+            { name: 'Alhambra Roja', description: 'Caña/Pinta', price: '2.50€/4.50€', image: 'images/roja.jpg' },
+            { name: 'Founders Porter', description: 'Caña/Pinta', price: '3.00€/5.50€', image: 'images/founders.jpg' },
+            { name: 'Franziskaner', description: 'Caña/Pinta', price: '2.20€/4.00€', image: 'images/franziskaner.jpg' }
         ],
         'Embotelladas': [
             { name: 'Mahou Clásica', description: 'Tercio', price: '2.20€', image: 'images/tercioestrella.jpg' },
@@ -43,32 +43,60 @@ const menuData = {
             { name: 'Sidra Maeloc Piña&Pera', description: 'Tercio', price: '2.50€', image: 'images/sidraperapiña.jpg' }
         ]
     },
-    copas: [
-        { name: 'Brugal Añejo', description: '', price: '', image: 'images/brugal.jpg' },
-        { name: 'Barceló Añejo', description: '', price: '', image: 'images/barcelo.jpg' },
-        { name: 'Legendario', description: '', price: '', image: 'images/legendario.jpg' },
-        { name: 'Bacardí', description: '', price: '', image: 'images/bacardi.jpg' },
-        { name: 'Havana Club', description: '', price: '', image: 'images/havana.jpg' },
-        { name: 'Larios 12', description: '', price: '', image: 'images/larios.jpg' },
-        { name: "Seagram's Dry Gin", description: '', price: '', image: 'images/seagrams.jpg' },
-        { name: "Citadelle Gin", description: '', price: '', image: 'images/citadelle.jpg' },
-        { name: "Puerto de Indias", description: '', price: '', image: 'images/puerto.jpg' },
-        { name: "Jack Daniel's Old No. 7", description: '', price: '', image: 'images/jackdaniels.jpg' },
-        { name: "Jack Daniel's Tennessee Apple", description: '', price: '', image: 'images/jackapple.jpg' },
-        { name: "Jameson Irish Whiskey", description: '', price: '', image: 'images/jameson.jpg' },
-        { name: "Eristoff", description: '', price: '', image: 'images/eristoff.jpg' },
-        { name: "Kiroff", description: '', price: '', image: 'images/kiroff.jpg' },
-        { name: "Absolut", description: '', price: '', image: 'images/absolut.jpg' },
-        { name: "J&B", description: '', price: '', image: 'images/jb.jpg' },
-        { name: "Queimada", description:'', price:'', image:'images/queimada.jpg' },
-        { name: "Tía María", description: '', price: '', image: 'images/tiamaria.jpg' },
-        { name: "Absenta", description: '', price: '', image: 'images/absenta.jpg' },
-    ]
+copas: [
+    // Licores y cremas (menor graduación)
+    { name: 'Tía María', description: '', price: '', image: 'images/tiamaria.jpg' },
+    { name: 'Malibú', description: '', price: '', image: 'images/malibu.jpg' },
+    { name: 'Mangaroca Batida de Coco', description: '', price: '', image: 'images/mangaroca.jpg' },
+    { name: 'Jagermeister', description: '', price: '', image: 'images/jager.jpg' },
+    { name: 'Licor Cincuenta y Siete', description: '', price: '', image: 'images/cincuentaysiete.jpg' },
+    { name: 'Cuaracao Blue', description: '', price: '', image: 'images/curacao.jpg' },
+    { name: 'Colosal Peppermint', description: '', price: '', image: 'images/peppermint.jpg' },
+
+    // Ron
+    { name: 'Bacardí', description: '', price: '', image: 'images/bacardi.jpg' },
+    { name: 'Havana Club', description: '', price: '', image: 'images/havana.jpg' },
+    { name: 'Barceló Añejo', description: '', price: '', image: 'images/barcelo.jpg' },
+    { name: 'Brugal Añejo', description: '', price: '', image: 'images/brugal.jpg' },
+    { name: 'Legendario', description: '', price: '', image: 'images/legendario.jpg' },
+
+    // Vodka
+    { name: 'Kiroff Caramelo', description: '', price: '', image: 'images/eristoff.jpg' },
+    { name: 'Kiroff Regaliz', description: '', price: '', image: 'images/kiroff.jpg' },
+    { name: 'Absolut', description: '', price: '', image: 'images/absolut.jpg' },
+
+    // Ginebra
+    { name: 'Puerto de Indias Rosa', description: '', price: '', image: 'images/puerto.jpg' },
+    { name: 'Puerto de Indias Verde', description: '', price: '', image: 'images/puerto.jpg' },
+    { name: 'Larios 12', description: '', price: '', image: 'images/larios.jpg' },
+    { name: "Seagram's Dry Gin", description: '', price: '', image: 'images/seagrams.jpg' },
+    { name: 'Citadelle Gin', description: '', price: '', image: 'images/citadelle.jpg' },
+
+    // Tequila
+    { name: 'Cachito Loco Tequila', description: '', price: '', image: 'images/cachito.jpg' },
+
+    // Whisky
+    { name: 'J&B', description: '', price: '', image: 'images/jb.jpg' },
+    { name: "Dewar's White Label", description: '', price: '', image: 'images/dewars.jpg' },
+    { name: 'Jameson Irish Whiskey', description: '', price: '', image: 'images/jameson.jpg' },
+    { name: 'Four Roses', description: '', price: '', image: 'images/fourroses.jpg' },
+    { name: "Jack Daniel's Old No. 7", description: '', price: '', image: 'images/jackdaniels.jpg' },
+    { name: "Jack Daniel's Tennessee Apple", description: '', price: '', image: 'images/jackapple.jpg' },
+
+    // Orujo / aguardiente
+    { name: 'Quemeira', description:'', price:'', image:'images/quemeira.jpg' },
+
+    // Absenta (mayor graduación)
+    { name: 'Absenta Diable Azul', description: '', price: '', image: 'images/absentaa.jpg' },
+    { name: 'Absenta Diable Negra', description: '', price: '', image: 'images/absentan.jpg' },
+    { name: 'Absenta Diable Verde', description: '', price: '', image: 'images/absentav.jpg' },
+]
+
 };
 
 // Textos informativos para cada sección (opcional)
 const sectionInfo = {
-    copas: ' Todas las copas normales a 6.00€, premium a 7.00€, y con Monster 7.50€.'
+    copas: ' Todas las copas normales a 6.00€, premium a 7.00€, y con Monster 7.50€. Chupitos a 1€.'
 };
 
 // Función para alternar secciones
